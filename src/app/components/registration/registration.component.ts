cca2a8a669bf09990c763ca8e2883fcfd22b040a
rohit001007 <rohit001007@gmail.com>
2021-01-21 19:31:28 +0530

    Added : New UI Of Registration and Login

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  form: FormGroup;
+  loading = false;
+  submitted = false;
 
   constructor() { }
 
